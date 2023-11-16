b31c2a39782518260faeda9054f96183983ca5fd
EveBaker <evebaker0162@gmail.com>
2023-11-16 14:00:19 -0600

    practice

---
+++ JQ2.js
@@ -0,0 +1,18 @@
+function func1()
+{
+    //$(element name).action()
+    //document.getElementByTageName(element name)
+
+    /*
+    changes the color to orange:
+    $("div").css('background-color','orange');
+    */
+
+    /*
+    changes the font of first div, p1 to italic)
+    $("#p1").css('font-style','italic');
+    */
+
+    //changes both of the box p's to itallic
+    $(".para").css("font-style","italic");
+}
